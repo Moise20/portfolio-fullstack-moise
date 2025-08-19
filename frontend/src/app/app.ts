d2f9b5c347c22f9ffbc5d65225d1092c4250aa31
PANA <essognim-moise.pana@soprasteria.com>
2025-08-19 16:05:29 +0200

    feat(nav): barre flottante + landing scroll; icônes; routes & styles

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './layout/navbar/navbar';
 import { Footer } from './layout/footer/footer';
+import { FloatingNav } from './layout/floating-nav/floating-nav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer,FloatingNav],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
