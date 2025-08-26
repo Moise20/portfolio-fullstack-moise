d13f81c56b92fb3815cbdc09d0ff0ccd20ee39eb
PANA <essognim-moise.pana@soprasteria.com>
2025-08-27 00:00:11 +0200

    modif du yml prod et du config des cors

--- backend/portfolio-api/src/main/java/com/moise/portfolio_api/config/CorsConfig.java
+++ backend/portfolio-api/src/main/java/com/moise/portfolio_api/config/CorsConfig.java
@@ -15,7 +15,16 @@ public class CorsConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration cfg = new CorsConfiguration();
-    cfg.setAllowedOrigins(allowedOrigins);
+    cfg.setAllowedOrigins(List.of(
+      "https://thriving-pixie-ffb547.netlify.app",
+      "https://portfolio-fullstack-moise.onrender.com",
+      "https://sparkly-salamander-21f217.netlify.app",
+      "https://wonderful-bombolone-333898.netlify.app",
+      "https://ton-domaine.netlify.app",
+      "https://ton-domaine.vercel.app"
+      
+    ));
+
     cfg.setAllowedMethods(List.of("GET","POST","PATCH","PUT","DELETE","OPTIONS"));
     cfg.setAllowedHeaders(List.of("*"));
     cfg.setAllowCredentials(true);
